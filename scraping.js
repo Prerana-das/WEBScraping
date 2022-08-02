0cd393a784ae48bfd095a9270b0b3a7d82f00586
Prerana_das <preranadas97@gmail.com>
2022-08-02 15:55:19 +0600

    dynamic content get

--- scraping.js
+++ scraping.js
@@ -4,14 +4,27 @@ const cheerio= require("cheerio");
 axios.get("https://www.jamesqquick.com/talks")
     .then((res) => {
 
-        const talks={};
+        const talks=[];
         // console.log('res',res.data)
         const $=cheerio.load(res.data);
         // console.log($('.card--content')
-        console.log($('.post-list')
-                        .children('a')
-                        .first()
-                        .attr('href'))
+        // console.log($('.post-list')
+        //                 .children('a')
+        //                 .first()
+        //                 .attr('href'))
                         // .text())
                         // .html())
+
+
+            $('.card').each((index,element)=>{
+               const title= $(element)
+                .children('.card--content')
+                .children('.card--title')
+                .first()
+                .text()
+                
+                console.log("title",title)
+                // talks[index]= {title};
+
+            })
     });
